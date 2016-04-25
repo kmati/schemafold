21c30cc591d6a346932b5a97a1ecb31a40688380
Kimanzi Mati <kimanzimati@kmatimac.local>
2016-04-25 14:54:49 -0400

    Added the getter computed field functionality

--- bootstrap/sample-models/Person.js
+++ bootstrap/sample-models/Person.js
@@ -1,7 +1,13 @@
 import { Address } from './Address';
 export const Person = {
 	id: Number,
-	firstName: { type: String, required: true },
-	lastName: { type: String, required: true },
-	address: Address
+	firstname: { type: String, required: true },
+	lastname: { type: String, required: true },
+	address: Address,
+	fullname: {
+		type: Function,
+		get: function () {
+			return this.firstname + ' ' + this.lastname;
+		}
+	}
 };
